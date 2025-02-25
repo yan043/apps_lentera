b45ef944ccc1091f14dd63b36f697004f31393fc
Mahdian ID <mahdian043@gmail.com>
2025-02-26 02:13:37 +0800

    alerts + error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>404 Page Not Found</title>
+
+    <link href="/theme/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/theme/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/theme/vendors/nprogress/nprogress.css" rel="stylesheet">
+
+    <link href="/theme/build/css/custom.min.css" rel="stylesheet">
+  </head>
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-12">
+          <div class="col-middle">
+            <div class="text-center text-center">
+              <h1 class="error-number">404</h1>
+              <h2>Sorry but we couldn't find this page</h2>
+              <p>This page you are looking for does not exist</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="/theme/vendors/jquery/dist/jquery.min.js"></script>
+   <script src="/theme/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/theme/vendors/fastclick/lib/fastclick.js"></script>
+    <script src="/theme/vendors/nprogress/nprogress.js"></script>
+    <script src="/theme/build/js/custom.min.js"></script>
+  </body>
+</html>
---
+++ resources/views/partial/alerts.blade.php
@@ -0,0 +1,10 @@
+@if ($errors->any())
+    @foreach ($errors->all() as $error)
+        <div class="alert alert-danger alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">×</span>
+            </button>
+            {{ $error }}
+        </div>
+    @endforeach
+@endif
